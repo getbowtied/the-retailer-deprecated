6c0b5cbdfb9189e038969d4cda6291ce39c685e7
Vanesa Reinerth <vanesa@getbowtied.com>
2019-07-01 13:18:17 +0300

    first commit

---
+++ README.txt
@@ -0,0 +1,18 @@
+=== The Retailer Deprecated Features ===
+Contributors: getbowtied, vanesareinerth, adrianlbs
+Requires at least: 5.0
+Tested up to: 5.2.2
+Stable tag: 1.0
+Requires PHP: 5.5.0
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+The Retailer Deprecated Features is a new plugin coming with the theme offering a fallback for old features that are no longer used.
+
+Deprecated:
+- WPBakery's Page Builder elements
+
+== Changelog ==
+
+= 1.0 =
+- Initial Version
---
+++ the-retailer-deprecated.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Plugin Name:       		The Retailer Deprecated Features
+ * Plugin URI:        		https://theretailer.wp-theme.design/
+ * Description:       		Old features of The Retailer theme that are no longer used.
+ * Version:           		1.0
+ * Author:            		GetBowtied
+ * Author URI:				https://getbowtied.com
+ * Text Domain:				the-retailer-deprecated
+ * Domain Path:				/languages/
+ * Requires at least: 		5.0
+ * Tested up to: 			5.2.2
+ *
+ * @package  The Retailer Deprecated
+ * @author   GetBowtied
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+} // Exit if accessed directly
+
+if ( ! function_exists( 'is_plugin_active' ) ) {
+    require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+}
+
+// Plugin Updater
+require 'core/updater/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://raw.githubusercontent.com/getbowtied/the-retailer-deprecated/master/core/updater/assets/plugin.json',
+	__FILE__,
+	'the-retailer-deprecated'
+);
+
+global $theme;
+$theme = wp_get_theme();
+$parent_theme = $theme->parent();
+
+if ( $theme->template == 'theretailer') {
+
+	include_once( dirname(__FILE__) . '/includes/shortcodes/shortcodes.php');
+	
+	// Add new WP shortcodes to VC
+	add_action( 'plugins_loaded', function() {
+
+		if ( defined(  'WPB_VC_VERSION' ) ) {
+			include_once( dirname(__FILE__) . '/includes/shortcodes/vc/shortcodes.php');
+		}
+	});
+}
